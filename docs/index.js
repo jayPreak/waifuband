1c6b28005386c9f5223abcc128a4b70dc8a06bfe
jayPreak <jayesh.preak2003@gmail.com>
2022-07-14 15:16:33 +0530

    styling idfk

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,9 @@
-console.log("yep");
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+        switch (this.innerHTML) {
+            case "w":
+                var goku = new Audio('sounds/')
+        }
+    })
+
+}
